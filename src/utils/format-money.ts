1745f710a44310ad6e33dedcc97d50a7cad45d31
jeperico <joaoeperico@gmail.com>
2025-10-28 18:34:39 -0300

    feat: src structure: interfaces && utils && services

---
+++ src/utils/format-money.ts
@@ -0,0 +1,20 @@
+
+
+export interface IFormatMoneyOptions {
+  currency?: string;
+  locale?: string;
+}
+
+const formatMoney = (
+  amount: number,
+  options: IFormatMoneyOptions = {}
+): string => {
+  const { currency = 'USD', locale = 'en-US' } = options;
+
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+  }).format(amount);
+};
+
+export default formatMoney;
